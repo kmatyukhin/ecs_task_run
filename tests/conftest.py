e20bc171fb8d905e728bc46dfa85ed5a19a2de2b
Konstantin Matyukhin <kmatyukhin@gmail.com>
2020-07-26 16:13:49 +0200

    Specify region name in boto3 tesing fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ from botocore.stub import Stubber
 
 @pytest.fixture(autouse=True)
 def sts_stub():
-    with Stubber(boto3.client("sts")) as stubber:
+    with Stubber(boto3.client("sts", region_name="us-west-2")) as stubber:
         yield stubber
         stubber.assert_no_pending_responses()
 
 
 @pytest.fixture(autouse=True)
 def ecs_stub():
-    with Stubber(boto3.client("ecs")) as stubber:
+    with Stubber(boto3.client("ecs", region_name="us-west-2")) as stubber:
         yield stubber
         stubber.assert_no_pending_responses()
